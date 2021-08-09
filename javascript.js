a165579877044f1862cb74b28dc4dbed79f6af56
sarvankm <sarvankm@code.edu.az>
2021-08-10 01:23:37 +0400

    Game Is Ready

--- javascript.js
+++ javascript.js
@@ -4,8 +4,10 @@ const input = document.getElementById("inp");
 const undos = document.querySelectorAll(".fa-undo-alt")
 document.getElementById("inp").value = -10;
 container.style.transform = "rotateZ(-10deg)";
+var RandomNumGlobal;
 spin.addEventListener("click", () => {
-    const RandomNum = Math.abs(Math.floor((Math.random() * 10) - 3));
+    const RandomNum = Math.abs(Math.floor(Math.random() * 10 - 1));
+    RandomNumGlobal = RandomNum;
     if (RandomNum === 0) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-260deg)";
@@ -52,7 +54,7 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s anm9"
             input.value = "-260"
         }
-    }
+    };
     if (RandomNum === 1) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-210deg)";
@@ -99,7 +101,7 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s Slice2Anm9"
             input.value = "-210"
         }
-    }
+    };
     if (RandomNum === 2) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-160deg)";
@@ -146,7 +148,7 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s Slice3Anm9"
             input.value = "-160"
         }
-    }
+    };
     if (RandomNum === 3) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-110deg)";
@@ -193,7 +195,7 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s Slice4Anm9"
             input.value = "-110"
         }
-    }
+    };
     if (RandomNum === 4) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-60deg)";
@@ -240,7 +242,7 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s Slice5Anm9"
             input.value = "-60"
         }
-    }
+    };
     if (RandomNum === 5) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(-10deg)";
@@ -288,7 +290,7 @@ spin.addEventListener("click", () => {
             input.value = "-10"
         }
 
-    }
+    };
     if (RandomNum === 6) {
         if (input.value === "-10") {
             container.style.transform = "rotateZ(40deg)";
@@ -335,32 +337,66 @@ spin.addEventListener("click", () => {
             container.style.animation = "4s Slice7Anm9"
             input.value = "40"
         }
-    }
+    };
     setTimeout(() => {
-        container.style.animation = ""
+        container.style.animation = "";
     }, 4000);
 });
-document.querySelectorAll(".colorOpacity").forEach((val)=>{
-    val.addEventListener("click",function(){
-    this.parentElement.children[3].innerHTML = parseInt(this.parentElement.children[3].innerHTML) + 1;
-    document.querySelector(".useramount").children[2].innerHTML = parseInt(document.querySelector(".useramount").children[2].innerHTML) - 5;
-    this.parentElement.children[1].style.display="block"
-});
-});
-undos.forEach((undo)=>{
-    undo.addEventListener("click",function(){
-        if (this.parentElement.children[3].innerHTML > 0) {
-            this.parentElement.children[3].innerHTML = parseInt(this.parentElement.children[3].innerHTML) - 1;
-            document.querySelector(".useramount").children[2].innerHTML = parseInt(document.querySelector(".useramount").children[2].innerHTML) + 5;
-            if (this.parentElement.children[3].innerHTML === "0") {
-                undo.style.display="none"
-            }
-
-        }
-   
+document.querySelectorAll(".colorOpacity").forEach((val) => {
+    val.addEventListener("click", function () {
+        this.parentElement.children[3].innerHTML = parseInt(this.parentElement.children[3].innerHTML) + 1;
+        document.querySelector(".useramount").children[3].innerHTML = parseInt(document.querySelector(".useramount").children[3].innerHTML) - 5;
+        this.parentElement.children[1].style.display = "block";
     });
 });
-document.querySelector(".spincontainer").addEventListener("click",function(){
-    document.querySelector(".bets").style.animation="8s forBets"
-    // document.querySelector(".bets").style.display="none"
-})
+undos.forEach((undo) => {
+    undo.addEventListener("click", function () {
+        if (this.parentElement.children[3].innerHTML > 0) {
+            this.parentElement.children[3].innerHTML = parseInt(this.parentElement.children[3].innerHTML) - 1;
+            document.querySelector(".useramount").children[3].innerHTML = parseInt(document.querySelector(".useramount").children[3].innerHTML) + 5;
+            if (this.parentElement.children[3].innerHTML === "0") {
+                undo.style.display = "none"
+            };
+        };
+    });
+});
+document.querySelector("#spin").addEventListener("click", function () {
+    const bets = document.querySelector(".bets");
+    this.style.pointerEvents = "none";
+    bets.style.animation = "5s forBets";
+    bets.style.position = "absolute";
+    bets.style.top = "400%";
+    setTimeout(() => {
+        this.style.pointerEvents = "auto";
+    }, 4000);
+    setTimeout(() => {
+        bets.style.position = "absolute";
+        bets.style.top = "100%";
+        bets.style.animation = "none";
+        bets.style.animation = "3s forBets2";
+        document.querySelectorAll(".betcount").forEach((text) => {
+            if (text.innerHTML != "0") {
+                if (parseInt(text.nextElementSibling.innerHTML) == RandomNumGlobal + 1) {
+                    setTimeout(() => {
+                        document.querySelector(".useramount").children[3].innerHTML = parseInt(document.querySelector(".useramount").children[3].innerHTML) + parseInt(text.nextElementSibling.innerHTML) * 5 + 5;
+                        Swal.fire({
+                            title: `CONGURATULATIONS </br> YOU WON ${parseInt(text.nextElementSibling.innerHTML) * 5 + 5} USD`,
+                            width: 600,
+                            timer: 3000,
+                            padding: '3em',
+                            background: 'white',
+                            backdrop: `
+                          rgba(0,0,123,0.4)
+                          url("assets/img/confetti.gif")
+                        `
+                        })
+                    }, 3500);
+                };
+            };
+            text.innerHTML = "0"
+        });
+        document.querySelectorAll(".fa-undo-alt").forEach((icon) => {
+            icon.style.display = "none";
+        });
+    }, 1000);
+});
